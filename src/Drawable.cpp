7d51d47ac606a59f8d080a51576dc3e41409eb6a
Miguel235711 <a01423629@itesm.mx>
2020-10-22 21:27:17 -0500

    drawable barely modified

--- src/Drawable.cpp
+++ src/Drawable.cpp
@@ -56,6 +56,7 @@ Drawable & Drawable::addRotation(Point p1, Point p2, GLfloat theta){
     print();
     std::cout << "after adding rotation\n";
     opGlobal->RotacionLibre(theta,{p1.x,p1.y,p1.z},{p2.x,p2.y,p2.z},A);
+
     print();
     return *this;
 }
